feb704da8db8a0cedbae5bd3f5edd686b5fd8e57
norogoth <norooth@gmail.com>
2021-04-25 19:09:35 -0700

    Commiting my package json files and some change with App.js

--- App.js
+++ App.js
@@ -1,35 +1,37 @@
 const http = require('http');
-const expwess = express();
-const psql = require('pg');
+const expwessReq = require('express');
+const expwess = expwessReq();
+const {Client} = require('pg');
 const cors = require('cors');
 const port = 8000;
 
 expwess.get('/', (req, res) => {
 	res.send('Server is up and running');
-}
+})
 
 expwess.listen(port, () => {
 	console.log(`listening in at http://localhost:${port}`);
-}
+})
 
 let data = null;
-const username = 'user';
-const password = 'password';
+const username = 'bismarck';
+const password = 'chongle33';
 
 const config = {
 	host: 'localhost',
 	user: username,
 	password: password,
+	database: 'mydb',
 	port: port,
 }
 
 const client = new Client(config);
-await client.connect( err => {
+client.connect(err => {
 	if (err) throw err;
 	else {
 		getData();
 	}
-);
+});
 
 function getData(sql) {
 	client
@@ -42,11 +44,12 @@ function getData(sql) {
 	.then(() => {
 		console.log('finished execution.');
 		process.exit();
-	}
-}
-const res = await client.query('SELECT stuff FROM things;', (err, res) => {
+	})
+const res = client.query(sql, (err, res) => {
 	if (err) throw err;
 	console.log(res);
 	client.end();
 });
+}
 
+getData('SELECT * FROM sample;');
